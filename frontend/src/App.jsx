c93039d7392f5f29c3c880a630cca680e0690288
Joris Nehls <joris.hendrik.nehls@gmail.com>
2026-01-01 16:29:19 +0100

    basic frontend react setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+      <h1>Notes App</h1>
+      <p>Frontend is ready</p>
+    </div>
+    </>
+  )
+}
+
+export default App
